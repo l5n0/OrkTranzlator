2eaaa2e2c7ea93c937f4d7335caefdb716e58237
Leandro <a@l5.lol>
2024-08-02 19:35:29 +0200

    20240802-1935

--- bot.py
+++ bot.py
@@ -23,13 +23,13 @@ class CustomHelpCommand(commands.HelpCommand):
 
     async def send_bot_help(self, mapping):
         embed = discord.Embed(title="OrkTranzlator Bot Help", color=discord.Color.green())
-        embed.add_field(name="!tl <text>", value="Translates the given text into Ork language.", inline=False)
-        embed.add_field(name="Example", value="!translate Hello friends", inline=False)
+        embed.add_field(name="çtl <text>", value="Translates the given text into Ork language.", inline=False)
+        embed.add_field(name="Example", value="çtl Hello friends", inline=False)
         await self.get_destination().send(embed=embed)
 
     async def send_command_help(self, command):
         embed = discord.Embed(title=f"{command.name} Command Help", color=discord.Color.green())
-        embed.add_field(name=f"!{command.name} {command.signature}", value=command.help, inline=False)
+        embed.add_field(name=f"ç{command.name} {command.signature}", value=command.help, inline=False)
         await self.get_destination().send(embed=embed)
 
 bot.help_command = CustomHelpCommand()
